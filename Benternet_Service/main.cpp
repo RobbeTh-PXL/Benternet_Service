6ced89895349fb732dc80fb2e2c84e242cc60db6
RobbeTh-PXL <12102842@student.pxl.be>
2024-03-29 22:18:48 +0100

    Debug/Test of service_translator

--- Benternet_Service/main.cpp
+++ Benternet_Service/main.cpp
@@ -1,15 +1,19 @@
 #include <QCoreApplication>
 
 #include "service_translator.h"
+#include "connection_zmq.h"
 
 int main(int argc, char *argv[])
 {
 	QCoreApplication a(argc, argv);
 
+	connection_zmq zmq_network;
 	service_translator translator;
 
-	translator.zmq_connect();
-	translator.zmq_listen();
+	translator.translate();
+
+	//zmq_network.connect();
+	//zmq_network.listen();
 
 	return a.exec();
 }

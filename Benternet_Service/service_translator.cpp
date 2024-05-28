ab7f711f0b8a9a9f20981278535e904a1b7daefd
RobbeTh-PXL <12102842@student.pxl.be>
2024-05-28 23:50:01 +0200

    Added command logic

--- Benternet_Service/service_translator.cpp
+++ Benternet_Service/service_translator.cpp
@@ -1,5 +1,7 @@
 #include "service_translator.h"
 #include <cpprest/http_client.h>
+#include <vector>
+#include <sstream>
 
 service_translator::service_translator() {}
 
@@ -67,4 +69,77 @@ std::string service_translator::translate(std::string q, std::string source, std
 	}
 
 	return translated_text;
+}
+
+std::string service_translator::langs(std::string language) {
+	std::cout << "[+] Fetching Languages Started" << std::endl;
+	web::http::client::http_client client(U(TRANS_SERVER));
+
+	// Prepare the request data
+	web::uri_builder builder(U(LANG_SERVER_ENDPOINT));
+
+	// Build the request URI
+	web::uri request_uri = builder.to_uri();
+
+	// Prepare the request headers
+	web::http::http_request request(web::http::methods::GET);
+	request.headers().set_content_type(U("application/x-www-form-urlencoded"));
+	request.headers().add(U("Accept"), U("application/json"));
+
+	// Set the request URI
+	request.set_request_uri(request_uri);
+
+	std::vector<std::string> languages;
+
+	try {
+		// Send the request asynchronously
+		client.request(request).then([](web::http::http_response response) {
+								   // Check if the request was successful (status code 200)
+								   if (response.status_code() == web::http::status_codes::OK) {
+									   // Extract and print the response body
+									   return response.extract_json();
+								   } else {
+									   // If the request failed, set an error
+									   throw std::runtime_error("Request failed with status code " + std::to_string(response.status_code()));
+								   }
+							   }).then([&languages, &language](web::json::value responseBody) {
+				// Check if the response is an array
+				if (responseBody.is_array()) {
+					for (auto& lang : responseBody.as_array()) {
+						// Check if the object has the field "code" with value "en"
+						if (lang.has_field(U(LANG_RESULT_LIST_ID)) && lang[U(LANG_RESULT_LIST_ID)].as_string() == U(language)) {
+							// Check if the object has the field "languages"
+							if (lang.has_field(U(LANG_RESULT_LIST_ITEMS)) && lang[U(LANG_RESULT_LIST_ITEMS)].is_array()) {
+								for (auto& language : lang[U(LANG_RESULT_LIST_ITEMS)].as_array()) {
+									languages.push_back(language.as_string());
+								}
+							}
+							break; // No need to continue once we've found the "en" entry
+						}
+					}
+				} else {
+					// Print an error message if the response does not contain a "translation" field
+					throw std::runtime_error("Translation not found in response");
+				}
+			}).wait();
+
+	} catch (const std::exception& e) {
+		// Handle exceptions thrown during asynchronous operations
+		std::cerr << "An error occurred: " << e.what() << std::endl;
+		// Set translated text to error
+		return e.what();
+	}
+
+	// Format the languages as a single string
+	std::ostringstream oss;
+	oss << "Languages for " << language << ":" << std::endl;
+	for (const auto& language : languages) {
+		oss << language << " ";
+	}
+	oss << std::endl;
+	return oss.str();
+}
+
+std::string service_translator::detect() {
+	return "Not yet implemented";
 }
--- Benternet_Service/service_translator.h
+++ Benternet_Service/service_translator.h
@@ -4,10 +4,18 @@
 #include <string>
 
 #define TRANS_SERVER "http://localhost:5000"
-#define TRANS_SERVER_ENDPOINT "/translate"
 
+#define TRANS_SERVER_ENDPOINT "/translate"
 #define TRANS_FORMAT "text"
-#define TRANS_RESULT_FIELD "translation"
+#define TRANS_RESULT_FIELD "translatedText"
+
+#define LANG_SERVER_ENDPOINT "/languages"
+#define LANG_RESULT_LIST_ID "code"
+#define LANG_RESULT_LIST_ITEMS "targets"
+
+#define DETECT_SERVER_ENDPOINT "/detect"
+#define DETECT_RESULT_FIELD_CONF "confidence"
+#define DETECT_RESULT_LIST_LANG "language"
 
 class service_translator
 {
@@ -16,6 +24,8 @@ public:
 	~service_translator();
 
 	std::string translate(std::string q, std::string source, std::string target);
+	std::string langs(std::string language);
+	std::string detect();
 };
 
 #endif // SERVICE_TRANSLATOR_H

005a96581371363fcf289136589708589b0a5b66
RobbeTh-PXL <12102842@student.pxl.be>
2024-05-30 11:32:41 +0200

    Added some functionality

--- Benternet_Client/mainwindow.h
+++ Benternet_Client/mainwindow.h
@@ -2,6 +2,13 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <zmq.hpp>
+
+#define ZMQ_SERVER_PUSH_ADRESS "tcp://localhost:24041"
+#define ZMQ_SERVER_SUB_ADRESS "tcp://localhost:24042"
+
+#define ZMQ_SUB_FILTER_1 "translate?"
+#define ZMQ_PUSH_FILTER_1 "translate!"
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -18,9 +25,28 @@ public:
 	~MainWindow();
 
 private slots:
-	void input_username_clickec();
+	void input_connect_clicked();
+	void input_translate_clicked();
+	void input_lang_source_selected();
 
 private:
 	Ui::MainWindow *ui;
+
+	std::string getUsername();
+	std::string getSourceLanguage();
+	void setTargetLanguage();
+	std::string getTextInput();
+
+	std::string username;
+	bool connected = false;
+	QList<QString> lang_list;
+
+	zmq::context_t ctx;
+	zmq::socket_t sub;
+	zmq::socket_t push;
+	zmq::message_t zmq_msg;
+
+	void incoming_msg();
+	void send_msg(std::string msg);
 };
 #endif // MAINWINDOW_H
